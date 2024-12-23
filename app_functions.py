42a8a7e083ae8ca09832c27729886c416c03d6c0
SathiyanarayananSK <Sathiyanarayanan.s1698@gmail.com>
2024-12-24 03:13:57 +1100

    SQL db data read ready. Project complete

--- app_functions.py
+++ app_functions.py
@@ -2,6 +2,10 @@ import smtplib, ssl
 import os
 import selectorlib
 import requests
+import sqlite3
+
+# Establish a connection and a cursor
+connection = sqlite3.connect("data.db")
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -38,12 +42,21 @@ def send_email(message):
         print("Email Sent!")
 
 def store(extracted):
-    with open("data.txt", "a") as file:
-        file.write(extracted + "\n")
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
 
 
 def read(extracted):
-    with open("data.txt", "r") as file:
-        content = file.readlines()
-    return content
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    print(rows)
+    return rows
 
--- main.py
+++ main.py
@@ -9,9 +9,10 @@ if __name__ == "__main__":
         scraped = app_functions.scrape(URL)
         extracted = app_functions.extract(scraped)
         print(extracted)
-        file_list = app_functions.read(extracted)
+
         if extracted != "No upcoming tours":
-            if extracted+"\n" not in file_list:
+            row = app_functions.read(extracted)
+            if not row:
                 app_functions.store(extracted)
                 app_functions.send_email(extracted)
         time.sleep(2)
